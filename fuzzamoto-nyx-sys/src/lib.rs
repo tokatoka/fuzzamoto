98167caafb5f2e4a9e9718d1b02cf01657012e86
dergoegge <n.goeggi@gmail.com>
2025-03-14 16:46:02 +0000

    cargo update and switch to rust 2024

--- fuzzamoto-nyx-sys/src/lib.rs
+++ fuzzamoto-nyx-sys/src/lib.rs
@@ -1,6 +1,6 @@
 use std::os::raw::{c_char, c_uchar};
 
-extern "C" {
+unsafe extern "C" {
     pub fn nyx_init() -> usize;
     pub fn nyx_get_fuzz_input(data: *const c_uchar, max_size: usize) -> usize;
     pub fn nyx_skip();
