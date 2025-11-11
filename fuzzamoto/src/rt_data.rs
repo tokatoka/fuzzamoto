b1859257f4a248d76da6a4b4792f9e98802e0d4b
tokatoka <tokazerkje@outlook.com>
2025-11-11 18:43:05 +0100

    fmt

--- fuzzamoto/src/rt_data.rs
+++ fuzzamoto/src/rt_data.rs
@@ -4,6 +4,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct RuntimeMetadata {
     // TODO: Add things that are needed
-    // for example: 
+    // for example:
     // txids: Vec<usize>
 }
