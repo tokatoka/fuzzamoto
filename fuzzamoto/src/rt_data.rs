4f543b18cd04ce92db3195509bc5cbb88d3aba5e
tokatoka <tokazerkje@outlook.com>
2025-11-11 18:31:39 +0100

    forgot to add file

---
+++ fuzzamoto/src/rt_data.rs
@@ -0,0 +1,9 @@
+/// The runtime data observed during the course of harness execution
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct RuntimeMetadata {
+    // TODO: Add things that are needed
+    // for example: 
+    // txids: Vec<usize>
+}
