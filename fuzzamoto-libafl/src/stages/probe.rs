458ccb005079807945ea9f3d39f0fe1fae887229
tokatoka <tokazerkje@outlook.com>
2025-11-17 12:47:33 +0100

    adding files

---
+++ fuzzamoto-libafl/src/stages/probe.rs
@@ -0,0 +1,81 @@
+use crate::input::IrInput;
+use fuzzamoto_ir::{Instruction, Operation, ProbeOperation};
+use libafl::{
+    HasMetadata,
+    executors::Executor,
+    fuzzer::Evaluator,
+    stages::{
+        Restartable, Stage,
+        mutational::{MutatedTransform, MutatedTransformPost},
+    },
+    state::{HasCorpus, HasCurrentTestcase},
+};
+
+pub struct ProbingStage {}
+
+impl ProbingStage {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<E, EM, S, Z> Stage<E, EM, S, Z> for ProbingStage
+where
+    E: Executor<EM, IrInput, S, Z>,
+    Z: Evaluator<E, EM, IrInput, S>,
+    S: HasMetadata + HasCorpus<IrInput> + HasCurrentTestcase<IrInput>,
+{
+    fn perform(
+        &mut self,
+        fuzzer: &mut Z,
+        executor: &mut E,
+        state: &mut S,
+        manager: &mut EM,
+    ) -> Result<(), libafl::Error> {
+        let mut testcase = state.current_testcase_mut()?.clone();
+        let Ok(mut input) = IrInput::try_transform_from(&mut testcase, state) else {
+            return Ok(());
+        };
+
+        // adding probing operation to the beginning and to the end of the instructions
+        let mut builder = fuzzamoto_ir::ProgramBuilder::new(input.ir().context.clone());
+        builder
+            .append(Instruction {
+                inputs: vec![],
+                operation: Operation::Probe(ProbeOperation::EnableRecv),
+            })
+            .expect("appending EnableProbe should always succeed");
+        builder
+            .append_all(input.ir().instructions.iter().cloned())
+            .expect("Partial append should always succeed if full append succeeded");
+        builder
+            .append(Instruction {
+                inputs: vec![],
+                operation: Operation::Probe(ProbeOperation::DisableRecv),
+            })
+            .expect("appending EnableProbe should always succeed");
+
+        let Ok(new_program) = builder.finalize() else {
+            return Ok(());
+        };
+        // now swap it with the new program
+        *input.ir_mut() = new_program;
+        let (untransformed, post) = input.try_transform_into(state)?;
+        // this will automatically put metadata into the feedback
+        log::info!("Doing Probing");
+        let (_, corpus_id) = fuzzer.evaluate_filtered(state, executor, manager, &untransformed)?;
+        post.post_exec(state, corpus_id)?;
+
+        Ok(())
+    }
+}
+
+impl<S> Restartable<S> for ProbingStage {
+    fn should_restart(&mut self, _state: &mut S) -> Result<bool, libafl::Error> {
+        Ok(true)
+    }
+
+    fn clear_progress(&mut self, _state: &mut S) -> Result<(), libafl::Error> {
+        Ok(())
+    }
+}
