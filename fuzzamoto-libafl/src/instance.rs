77a63514b8c913a1870a422342d846fa4fa83f97
tokatoka <tokazerkje@outlook.com>
2026-01-08 16:31:11 +0100

    dump weights to output directory

--- fuzzamoto-libafl/src/instance.rs
+++ fuzzamoto-libafl/src/instance.rs
@@ -32,10 +32,15 @@ use libafl::{
     state::{HasCorpus, HasMaxSize, HasRand, StdState},
 };
 use libafl_bolts::{
-    HasLen, Named, current_nanos,
+    HasLen, Named,
+    core_affinity::CoreId,
+    current_nanos,
     rands::{Rand, StdRand},
-    tuples::{Handled, tuple_list},
+    tuples::{Handled, NamedTuple, tuple_list},
 };
+
+use std::collections::BTreeMap;
+
 use libafl_nyx::{executor::NyxExecutor, helper::NyxHelper, settings::NyxSettings};
 use rand::{SeedableRng, rngs::SmallRng};
 use typed_builder::TypedBuilder;
@@ -75,6 +80,29 @@ pub struct Instance<'a, EM> {
 }
 
 const AUX_BUFFER_SIZE: usize = 0x20000;
+
+fn log_weights<MT>(
+    options: &FuzzerOptions,
+    id: CoreId,
+    mutations: &MT,
+    weights: &[f32],
+) -> Result<(), Error>
+where
+    MT: NamedTuple,
+{
+    let names = mutations.names();
+    let map: BTreeMap<Cow<'_, str>, f32> = names
+        .into_iter()
+        .zip(weights.into_iter().copied())
+        .collect();
+
+    let config_path = options.output_dir(id).join("config.json");
+    let file = std::fs::File::create(config_path)?;
+    let writer = std::io::BufWriter::new(file);
+    serde_json::to_writer_pretty(writer, &map)
+        .map_err(|_| Error::serialize("Failed to serialize weights".to_string()))?;
+    Ok(())
+}
 
 impl<EM> Instance<'_, EM>
 where
@@ -373,9 +401,14 @@ where
                 IrGenerator::new(BlockTxnGenerator::default(), rng.clone())
             ),
         ];
+        log_weights(
+            &self.options,
+            self.client_description.core_id(),
+            &mutations,
+            &weights,
+        )?;
 
         let mutator = TuneableScheduledMutator::new(&mut state, mutations);
-
         let sum = weights.iter().sum::<f32>();
         debug_assert_eq!(mutator.mutations().len(), weights.len());
 
